2397d47508b3d62d22da0a5fb4c06c3df3c0794a
Pietro Madril <pietromotta.madril@gmail.com>
2025-04-12 20:29:12 -0300

    w4 assingmen youtube

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        List<Video> videos = new List<Video>();
+
+        //First video
+        Video video1 = new Video("How train your dog", "Pinscher", 587);
+        video1.Comments.Add(new Comment("Alice", "Soo cute!"));
+        video1.Comments.Add(new Comment("Spongebob", "Now i know how help my puppy."));
+        video1.Comments.Add(new Comment("Charlie", "That work with bigger dogs??"));
+        videos.Add(video1);
+
+        //Second video
+        Video video2 = new Video("Video editing", "Editor-3000", 360);
+        video2.Comments.Add(new Comment("Dan", "Thank you!"));
+        video2.Comments.Add(new Comment("Eliza", "Can you did a tutorial about VFX?"));
+        video2.Comments.Add(new Comment("Roy", "That fade effect is awasome!"));
+        videos.Add(video2);
+
+        //Third video
+        Video video3 = new Video("Tracking your health", "Personality", 540);
+        video3.Comments.Add(new Comment("Gina", "I love it."));
+        video3.Comments.Add(new Comment("Hannah", "Can you share your workout schedule?"));
+        video3.Comments.Add(new Comment("Ivan", "It's a wonderful starting point."));
+        videos.Add(video3);
+        
+        foreach (var video in videos)
+        {
+            video.DisplayVideoDetails();
+        }
     }
 }
---
+++ week04/YouTubeVideos/Video.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+
+public class Video
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int LengthInSeconds { get; set; }
+    public List<Comment> Comments { get; set; } = new List<Comment>();
+
+    public Video(string title, string author, int lengthInSeconds)
+    {
+        Title = title;
+        Author = author;
+        LengthInSeconds = lengthInSeconds;
+    }
+
+    public int GetNumberOfComments()
+    {
+        return Comments.Count;
+    }
+    public void DisplayVideoDetails()
+    {
+        Console.WriteLine($"Title: {Title}");
+        Console.WriteLine($"Author: {Author}");
+        Console.WriteLine($"Length: {LengthInSeconds} seconds");
+        Console.WriteLine($"Number of Comments: {GetNumberOfComments()}");
+
+        Console.WriteLine("Comments:");
+        foreach (var comment in Comments)
+        {
+            Console.WriteLine($"- {comment.CommenterName}: {comment.CommentText}");
+        }
+        Console.WriteLine();
+    }
+}
